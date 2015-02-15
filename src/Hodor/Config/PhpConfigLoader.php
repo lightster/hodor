676bdc9abf1e800dd10a1d2b74e7c87862299603
Matt Light <matt.light@lightdatasys.com>
2015-02-15 00:06:38 -0800

    Add PhpConfigLoader class

---
+++ src/Hodor/Config/PhpConfigLoader.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Hodor\Config;
+
+use Hodor\Config;
+
+use Exception;
+
+class PhpConfigLoader
+{
+    /**
+     * @param  string $file_path
+     * @return \Hodor\Config
+     */
+    public function loadFromFile($file_path)
+    {
+        if (!file_exists($file_path)) {
+            throw new Exception("Config file '{$file_path}' does not exist.");
+        }
+
+        $config_array = require $file_path;
+
+        if (!is_array($config_array)) {
+            throw new Exception("Config file '{$file_path}' does not return a PHP array.");
+        }
+
+        return new Config($config_array);
+    }
+}
---
+++ tests/src/Hodor/Config/PhpConfigLoaderTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Hodor\Config;
+
+use PHPUnit_Framework_TestCase;
+
+class PhpConfigLoaderTest extends PHPUnit_Framework_TestCase
+{
+    private $loader;
+
+    public function setUp()
+    {
+        $this->loader = new PhpConfigLoader();
+    }
+
+    /**
+     * @expectedException \Exception
+     */
+    public function testLoadFromNonExistentFile()
+    {
+        $this->loader->loadFromFile(__DIR__ . '/PhpConfigDoesNotExist.php');
+    }
+
+    /**
+     * @expectedException \Exception
+     */
+    public function testLoadFromInvalidFile()
+    {
+        $this->loader->loadFromFile(__DIR__ . '/PhpConfigIsInvalid.php');
+    }
+
+    public function testLoadFromFile()
+    {
+        $this->assertInstanceOf(
+            '\Hodor\Config',
+            $this->loader->loadFromFile(__DIR__ . '/PhpConfig.php')
+        );
+    }
+}
