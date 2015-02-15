5670da8a860519d46ccdcd1beb46f77a3463a9aa
Matt Light <matt.light@lightdatasys.com>
2015-02-15 01:17:16 -0800

    Add Config\LoaderFacade

---
+++ src/Hodor/Config/LoaderFacade.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Hodor\Config;
+
+class LoaderFacade
+{
+    /**
+     * @var \Hodor\LoaderFactory
+     */
+    private static $loader_factory;
+
+    /**
+     * @param  string $file_path
+     * @return \Hodor\Config
+     */
+    public static function loadFromFile($file_path)
+    {
+        self::getLoaderFactory()->loadFromFile($file_path);
+    }
+
+    /**
+     * @param \Hodor\Config\LoaderFactory $loader_factory
+     */
+    public static function setLoaderFactory(LoaderFactory $loader_factory)
+    {
+        self::$loader_factory = $loader_factory;
+    }
+
+    /**
+     * @return \Hodor\Config\LoaderFactory
+     */
+    private static function getLoaderFactory()
+    {
+        if (self::$loader_factory) {
+            return self::$loader_factory;
+        }
+
+        self::$loader_factory = new LoaderFactory();
+
+        return self::$loader_factory;
+    }
+}
---
+++ tests/src/Hodor/Config/LoaderFacadeTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Hodor\Config;
+
+use PHPUnit_Framework_TestCase;
+
+class LoaderFacadeTest extends PHPUnit_Framework_TestCase
+{
+    public function testFacadeCallsLoaderFactoryLoadFromFile()
+    {
+        $file_path = __DIR__ . '/PhpConfig.php';
+
+        $loader_factory = $this->getMockBuilder('\Hodor\Config\LoaderFactory')
+            ->setMethods(['loadFromFile'])
+            ->getMock();
+        $loader_factory->expects($this->once())
+            ->method('loadFromFile')
+            ->with(
+                $file_path
+            );
+
+        LoaderFacade::setLoaderFactory($loader_factory);
+        LoaderFacade::loadFromFile($file_path);
+    }
+}
