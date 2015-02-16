2702f541994e8c5c0d5632ba5f5762f206275513
Matt Light <matt.light@lightdatasys.com>
2015-02-16 06:26:21 -0800

    More braindumping of DB adapter interface

--- src/Hodor/Database/AdapterInterface.php
+++ src/Hodor/Database/AdapterInterface.php
@@ -5,8 +5,17 @@ namespace Hodor\Database;
 interface AdapterInterface
 {
     public function createJob($job);
+
+    public function getJobsToRun();
+
     public function markJobAsStarted($job);
+
     public function markJobAsCompleted($job);
     public function markJobAsFailed($job);
+
     public function getPhpmigAdapter();
+
+    public function beginTransaction();
+    public function commitTransaction();
+    public function rollbackTransaction();
 }
