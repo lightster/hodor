077ce2386420bfe265d62c45f3a8f7e5a0140293
Matt Light <matt.light@illuminateed.com>
2015-03-08 15:43:19 -0700

    refactor(bin): use job-queue queue-factory in job-worker
    Instead of directly accessing the message-queue queue-factory,
    the job-worker now goes through the job-queue queue-factory.

--- bin/job-worker.php
+++ bin/job-worker.php
@@ -5,13 +5,12 @@ require_once __DIR__ . '/../bootstrap.php';
 
 use Hodor\Command\Arguments as Arguments;
 use Hodor\Config\LoaderFacade as Config;
-use Hodor\MessageQueue\QueueFactory as QueueFactory;
-use Hodor\WorkerQueue;
+use Hodor\JobQueue\QueueFactory as QueueFactory;
 
 $args = new Arguments();
 $config = Config::loadFromFile($args->getConfigFile());
 
 $queue_factory = new QueueFactory($config);
-$worker_queue = new WorkerQueue($queue_factory->getWorkerQueue('default'));
+$worker_queue = $queue_factory->getWorkerQueue('default');
 
 $worker_queue->runNext();
