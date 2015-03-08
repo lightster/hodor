7ea92620534250748d5b984c67fbdbf646acc050
Matt Light <matt.light@illuminateed.com>
2015-03-07 16:20:05 -0800

    feat(config): load config based on CLI argument

--- bin/job-worker.php
+++ bin/job-worker.php
@@ -3,11 +3,13 @@
 
 require_once __DIR__ . '/../bootstrap.php';
 
+use Hodor\Command\Arguments as Arguments;
 use Hodor\Config\LoaderFacade as Config;
 use Hodor\MessageQueue\QueueFactory as QueueFactory;
 use Hodor\WorkerQueue;
 
-$config = Config::loadFromFile(__DIR__ . '/../config/config.php');
+$args = new Arguments();
+$config = Config::loadFromFile($args->getConfigFile());
 
 $queue_factory = new QueueFactory($config);
 $worker_queue = new WorkerQueue($queue_factory->getWorkerQueue('default'));
---
+++ src/Hodor/Command/Arguments.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Hodor\Command;
+
+use Exception;
+
+class Arguments
+{
+    /**
+     * @var array
+     */
+    private $loaded_arguments;
+
+    /**
+     * @return string
+     */
+    public function getConfigFile()
+    {
+        return $this->getRequiredArgument('config');
+    }
+
+    /**
+     * @return string
+     */
+    public function getQueueName()
+    {
+        return $this->getRequiredArgument('config');
+    }
+
+    /**
+     * @param  string $name
+     * @return string
+     */
+    private function getRequiredArgument($name)
+    {
+        $this->processArguments();
+
+        if (empty($this->loaded_arguments[$name])) {
+            throw new Exception("Argument '{$name}' is required.");
+        }
+
+        return $this->loaded_arguments[$name];
+    }
+
+    private function processArguments()
+    {
+        if ($this->loaded_arguments) {
+            return;
+        }
+
+        $args = getopt(
+            'c:q:',
+            [
+                'config:',
+                'queue:',
+            ]
+        );
+
+        if (!empty($args['config'])) {
+            $this->loaded_arguments['config'] = $args['config'];
+        } elseif (!empty($args['c'])) {
+            $this->loaded_arguments['config'] = $args['c'];
+        }
+
+        if (!empty($args['queue'])) {
+            $this->loaded_arguments['queue'] = $args['queue'];
+        } elseif (!empty($args['q'])) {
+            $this->loaded_arguments['queue'] = $args['q'];
+        }
+    }
+}
