cc9c8d749b33053b8a56f7905430bf08c23ad4c8
Matt Light <matt.light@illuminateed.com>
2015-03-09 07:55:14 -0700

    feat(config): Add config.dist.php as an example

---
+++ config/dist/config.dist.php
@@ -0,0 +1,25 @@
+<?php
+return [
+    'database' => [
+        'type' => 'pgsql',
+        'dsn'  => 'host=localhost user=hodor dbname=hodor',
+    ],
+    'queue_defaults' => [
+        'host'         => '127.0.0.1',
+        'port'         => 5672,
+        'username'     => 'guest',
+        'password'     => 'guest',
+        'queue_prefix' => 'hodor-'
+    ],
+    'worker_queue_defaults' => [
+        'queue_prefix' => 'hodor-worker-'
+    ],
+    'worker_queues' => [
+        'default' => [
+            'workers_per_server' => 10,
+        ],
+    ],
+    'job_runner' => function($name, $params) {
+        var_dump($name, $params);
+    },
+];
