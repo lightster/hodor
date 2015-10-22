48c235b0d4d95d084c0a4584f359d7cdd82a4082
Matt Light <matt.light@lightdatasys.com>
2015-10-22 16:16:20 -0700

    We need a DB schema for the superqueuer to use
    Create a schema for tracking buffered jobs, super
    queued jobs, completed jobs, and failed jobs
    
    Issue #2

---
+++ migrations/postgres/20151022112749_CreateInitialSchema.php
@@ -0,0 +1,111 @@
+<?php
+
+use Hodor\Database\AdapterInterface as DbAdapterInterface;
+use Hodor\Database\Phpmig\Migration;
+
+class CreateInitialSchema extends Migration
+{
+    /**
+     * @param DbAdapterInterface $db
+     * @return void
+     */
+    protected function transactionalUp(DbAdapterInterface $db)
+    {
+        $sql = <<<SQL
+CREATE TABLE buffered_jobs
+(
+    buffered_job_id SERIAL PRIMARY KEY,
+    queue_name VARCHAR NOT NULL,
+    job_name VARCHAR NOT NULL,
+    job_params JSON NOT NULL,
+    job_rank INT NOT NULL DEFAULT 5,
+    run_after TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    buffered_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    buffered_from VARCHAR NOT NULL,
+    inserted_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    inserted_from VARCHAR NOT NULL
+);
+
+CREATE TABLE superqueued_jobs
+(
+    superqueued_job_id SERIAL PRIMARY KEY,
+    buffered_job_id INT NOT NULL,
+    queue_name VARCHAR NOT NULL,
+    job_name VARCHAR NOT NULL,
+    job_params JSON NOT NULL,
+    job_rank INT NOT NULL DEFAULT 5,
+    run_after TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    buffered_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    buffered_from VARCHAR NOT NULL,
+    inserted_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    inserted_from VARCHAR NOT NULL,
+    superqueued_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    superqueued_from VARCHAR NOT NULL
+);
+
+CREATE TABLE successful_jobs
+(
+    successful_job_id SERIAL PRIMARY KEY,
+    buffered_job_id INT NOT NULL,
+    queue_name VARCHAR NOT NULL,
+    job_name VARCHAR NOT NULL,
+    job_params JSON NOT NULL,
+    job_rank INT NOT NULL DEFAULT 5,
+    run_after TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    buffered_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    buffered_from VARCHAR NOT NULL,
+    inserted_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    inserted_from VARCHAR NOT NULL,
+    superqueued_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    superqueued_from VARCHAR NOT NULL,
+    started_running_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    started_running_from VARCHAR NOT NULL,
+    finished_running_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    finished_running_from VARCHAR NOT NULL,
+    dequeued_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    dequeued_from VARCHAR NOT NULL
+);
+
+CREATE TABLE failed_jobs
+(
+    failed_job_id SERIAL PRIMARY KEY,
+    buffered_job_id INT NOT NULL,
+    queue_name VARCHAR NOT NULL,
+    job_name VARCHAR NOT NULL,
+    job_params JSON NOT NULL,
+    job_rank INT NOT NULL DEFAULT 5,
+    run_after TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    buffered_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    buffered_from VARCHAR NOT NULL,
+    inserted_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    inserted_from VARCHAR NOT NULL,
+    superqueued_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    superqueued_from VARCHAR NOT NULL,
+    started_running_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    started_running_from VARCHAR NOT NULL,
+    finished_running_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    finished_running_from VARCHAR NOT NULL,
+    dequeued_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    dequeued_from VARCHAR NOT NULL
+);
+SQL;
+
+        $db->queryMultiple($sql);
+    }
+
+    /**
+     * @param DbAdapterInterface $db
+     * @return void
+     */
+    protected function transactionalDown(DbAdapterInterface $db)
+    {
+        $sql = <<<SQL
+DROP TABLE buffered_jobs;
+DROP TABLE superqueued_jobs;
+DROP TABLE successful_jobs;
+DROP TABLE failed_jobs;
+SQL;
+
+        $db->queryMultiple($sql);
+    }
+}
