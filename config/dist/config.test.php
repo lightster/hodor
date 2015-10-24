15844078f838e72f8da778fec98e9b3f78596f58
Matt Light <matt.light@lightdatasys.com>
2015-10-23 22:35:33 -0700

    Add a test that requires a rabbitmq server

--- config/dist/config.test.php
+++ config/dist/config.test.php
@@ -6,5 +6,12 @@ return [
                 'dsn' => 'host=localhost',
             ],
         ],
+        'rabbitmq' => [
+            'host'            => '127.0.0.1',
+            'port'            => 5672,
+            'username'        => 'guest',
+            'password'        => 'guest',
+            'queue_prefix'    => 'test-hodor-',
+        ],
     ],
 ];
---
+++ tests/src/Hodor/MessageQueue/QueueFactoryTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Hodor\MessageQueue;
+
+use Exception;
+use PHPUnit_Framework_TestCase;
+
+class QueueFactoryTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var array
+     */
+    private $config;
+
+    /**
+     * @var QueueFactory
+     */
+    private $queue_factory;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $config_path = __DIR__ . '/../../../../config/config.test.php';
+        if (!file_exists($config_path)) {
+            throw new Exception("'{$config_path}' not found");
+        }
+
+        $this->config = require $config_path;
+        $this->queue_factory = new QueueFactory();
+    }
+
+    public function testQueueCanBeGenerated()
+    {
+        $config_template = $this->config['test']['rabbitmq'];
+        $config = [
+            'host'       => $config_template['host'],
+            'port'       => $config_template['port'],
+            'username'   => $config_template['username'],
+            'password'   => $config_template['password'],
+            'queue_name' => $config_template['queue_prefix'] . uniqid(),
+        ];
+
+        $this->assertInstanceOf(
+            '\Hodor\MessageQueue\Queue',
+            $this->queue_factory->getQueue($config)
+        );
+    }
+}
