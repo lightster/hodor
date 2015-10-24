1b144d7e129efc403f7d669187d832d04283cada
Matt Light <matt.light@lightdatasys.com>
2015-10-23 22:09:55 -0700

    Consolidate dist configs into config/dist

--- bin/test-publisher.php
+++ bin/test-publisher.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Hodor\JobQueueFacade as Q;
 
-Q::setConfigFile(__DIR__ . '/../config/config.php');
+Q::setConfigFile(__DIR__ . '/../config/config.test.php');
 Q::push(
     'some_job_name',
     ['some', 'cool', 'values', date('Y-m-d h:i:s')],
