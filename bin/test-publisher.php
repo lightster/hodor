8a9cc81313c1f9fe1c70a14b0d61219386bd864e
Matt Light <matt.light@lightdatasys.com>
2015-11-05 08:28:05 -0800

    Add config path and queue name args to test-publisher

--- bin/test-publisher.php
+++ bin/test-publisher.php
@@ -4,10 +4,15 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Hodor\JobQueueFacade as Q;
+use Hodor\Command\Arguments as Arguments;
 
-Q::setConfigFile(__DIR__ . '/../config/config.test.php');
+$args = new Arguments();
+$config_file = $args->getConfigFile();
+$queue_name = $args->getQueueName();
+
+Q::setConfigFile($config_file);
 Q::push(
     'some_job_name',
     ['some', 'cool', 'values', date('Y-m-d h:i:s')],
-    ['queue_name' => 'default']
+    ['queue_name' => $queue_name]
 );
