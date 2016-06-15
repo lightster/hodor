b6714999c1d7e29bdefbabd321229a2904e9f8c3
Matt Light <matt.light@lightdatasys.com>
2016-06-14 23:17:05 -0700

    Generate adapter factory just in time in Testing\Config
    The Config constructor requires an AdapterFactory and the
    AdapterFactory constructor requires a Config. Up until now
    the two testing classes have not yet needed to be used with
    each other. We may want to stop supplying the adapter
    factory through the config object, but for now let's use a
    callable to generate the adapter factory so the Config
    constructor does not directly require an AdapterFactory.

--- tests/src/Hodor/MessageQueue/Adapter/Amqp/ConfigProvider.php
+++ tests/src/Hodor/MessageQueue/Adapter/Amqp/ConfigProvider.php
@@ -27,7 +27,9 @@ class ConfigProvider
      */
     public function getConfigAdapter(array $queues, array $config_overrides = [])
     {
-        $config = new Config($this->test_case->getMock('Hodor\MessageQueue\Adapter\FactoryInterface'));
+        $config = new Config(function () {
+            return $this->test_case->getMock('Hodor\MessageQueue\Adapter\FactoryInterface');
+        });
         foreach ($queues as $queue_key => $queue_config) {
             $config->addQueueConfig($queue_key, array_merge($queue_config, $config_overrides));
         }
