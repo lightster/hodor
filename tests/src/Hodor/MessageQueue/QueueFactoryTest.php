b83e19dfed329d365508237d48287b741162e269
Matt Light <matt.light@lightdatasys.com>
2016-06-20 23:30:00 -0700

    Complete testing of MessageQueue\QueueFactory
    ... and for that matter, all of MessageQueue!!

--- tests/src/Hodor/MessageQueue/QueueFactoryTest.php
+++ tests/src/Hodor/MessageQueue/QueueFactoryTest.php
@@ -3,8 +3,9 @@
 namespace Hodor\MessageQueue;
 
 use Exception;
-use Hodor\MessageQueue\Adapter\Amqp\Factory;
-use Hodor\MessageQueue\Adapter\Config;
+use Hodor\MessageQueue\Adapter\Amqp\ConfigProvider;
+use Hodor\MessageQueue\Adapter\Testing\Factory;
+use Hodor\MessageQueue\Adapter\Testing\Config;
 use PHPUnit_Framework_TestCase;
 
 /**
@@ -13,61 +14,141 @@ use PHPUnit_Framework_TestCase;
 class QueueFactoryTest extends PHPUnit_Framework_TestCase
 {
     /**
+     * @var Config
+     */
+    private $config;
+
+    /**
      * @covers ::__construct
      * @covers ::getQueue
-     * @covers ::getAdapterFactory
+     * @covers ::<private>
      */
     public function testQueueCanBeGenerated()
     {
         $this->assertInstanceOf(
             '\Hodor\MessageQueue\Queue',
-            $this->generateQueueFactory()->getQueue('worker-default')
+            $this->generateQueueFactory()->getQueue('fast_jobs')
         );
     }
 
     /**
      * @covers ::__construct
      * @covers ::getQueue
-     * @covers ::getAdapterFactory
+     * @covers ::<private>
      */
     public function testQueueIsReusedIfReferredToMultipleTimes()
     {
         $queue_factory = $this->generateQueueFactory();
 
         $this->assertSame(
-            $queue_factory->getQueue('worker-default'),
-            $queue_factory->getQueue('worker-default')
+            $queue_factory->getQueue('fast_jobs'),
+            $queue_factory->getQueue('fast_jobs')
         );
     }
 
-    public function queueConfigProvider()
+    /**
+     * @covers ::__construct
+     * @covers ::getQueue
+     * @covers ::beginBatch
+     * @covers ::<private>
+     * @expectedException Exception
+     */
+    public function testMessagesPushedInsideOfBatchAreNotImmediatelyProduced()
     {
-        $config_path = __DIR__ . '/../../../../config/config.test.php';
-        if (!file_exists($config_path)) {
-            throw new Exception("'{$config_path}' not found");
-        }
+        $queue_factory = $this->generateQueueFactory();
 
-        $config = require $config_path;
-        $config_template = $config['test']['rabbitmq'];
+        $queue_factory->beginBatch();
+        $queue = $queue_factory->getQueue('fast_jobs');
 
-        return [
-            'host'        => $config_template['host'],
-            'port'        => $config_template['port'],
-            'username'    => $config_template['username'],
-            'password'    => $config_template['password'],
-            'queue_name'  => $config_template['queue_prefix'] . uniqid(),
-            'fetch_count' => 1,
-        ];
+        $queue->push('');
+        $queue->consume(function () {
+            $this->fail('No messages should have been pushed.');
+        });
     }
 
+    /**
+     * @covers ::__construct
+     * @covers ::getQueue
+     * @covers ::beginBatch
+     * @covers ::publishBatch
+     * @covers ::<private>
+     */
+    public function testPushingMessagesToAQueueRetrievedBeforeStartingABatchAreAvailableAfterPublishingTheBatch()
+    {
+        $expected = __METHOD__;
+
+        $queue_factory = $this->generateQueueFactory();
+
+        $queue = $queue_factory->getQueue('fast_jobs');
+        $queue_factory->beginBatch();
+        $queue->push($expected);
+        $queue_factory->publishBatch();
+
+        $queue->consume(function (IncomingMessage $message) use ($expected) {
+            $this->assertSame($expected, $message->getContent());
+        });
+    }
+
+    /**
+     * @covers ::__construct
+     * @covers ::getQueue
+     * @covers ::beginBatch
+     * @covers ::publishBatch
+     * @covers ::<private>
+     */
+    public function testPushingMessagesToAQueueRetrievedAfterStartingABatchAreAvailableAfterPublishingTheBatch()
+    {
+        $expected = __METHOD__;
+
+        $queue_factory = $this->generateQueueFactory();
+
+        $queue_factory->beginBatch();
+        $queue = $queue_factory->getQueue('fast_jobs');
+        $queue->push($expected);
+        $queue_factory->publishBatch();
+
+        $queue->consume(function (IncomingMessage $message) use ($expected) {
+            $this->assertSame($expected, $message->getContent());
+        });
+    }
+
+    /**
+     * @covers ::__construct
+     * @covers ::getQueue
+     * @covers ::beginBatch
+     * @covers ::discardBatch
+     * @covers ::<private>
+     * @expectedException Exception
+     */
+    public function testBatchedMessagesCanBeDiscarded()
+    {
+        $expected = __METHOD__;
+
+        $queue_factory = $this->generateQueueFactory();
+
+        $queue_factory->beginBatch();
+        $queue = $queue_factory->getQueue('fast_jobs');
+        $queue->push($expected);
+        $queue_factory->discardBatch();
+
+        $queue->consume(function () {
+            $this->fail('All messages should have been discarded.');
+        });
+    }
+
+    /**
+     * @return QueueFactory
+     */
     private function generateQueueFactory()
     {
-        $config_adapter = $this->getMock('\Hodor\MessageQueue\Adapter\ConfigInterface');
-        $config_adapter->method('getAdapterFactory')
-            ->willReturn(new Factory($config_adapter));
-        $config_adapter->method('getQueueConfig')
-            ->willReturn($this->queueConfigProvider());
+        $config_provider = new ConfigProvider($this);
 
-        return new QueueFactory($config_adapter);
+        $config = new Config(function (Config $config) {
+            return new Factory($config);
+        });
+        $config->addQueueConfig('fast_jobs', $config_provider->getQueueConfig());
+        $config->addQueueConfig('slow_jobs', $config_provider->getQueueConfig());
+
+        return new QueueFactory($config);
     }
 }
