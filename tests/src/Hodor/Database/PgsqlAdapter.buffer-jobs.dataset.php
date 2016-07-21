bf497ee45ca028792921dd5d056b05897d6a2cb5
Matt Light <matt.light@lightdatasys.com>
2016-07-21 00:31:38 -0700

    Separate bufferJob and queueJob testing
    We are going to separate the functionality into separate
    classes, so having the tests separate will make refactoring
    easier

---
+++ tests/src/Hodor/Database/PgsqlAdapter.buffer-jobs.dataset.php
@@ -0,0 +1,74 @@
+<?php
+
+return [
+    // most basic scenario
+    [
+        // buffered jobs
+        [
+            ['name' => '1'],
+            ['name' => '2'],
+        ],
+        // expected job order
+        ['1', '2'],
+    ],
+    // rank dictates queue order
+    [
+        // buffered jobs
+        [
+            ['name' => '1', 'job_rank' => 5],
+            ['name' => '2', 'job_rank' => 1],
+        ],
+        // expected job order
+        ['2', '1'],
+    ],
+    // mutex only allows one job to be queued
+    [
+        // buffered jobs
+        [
+            ['name' => '1', 'mutex_id' => 'a'],
+            ['name' => '2', 'mutex_id' => 'a'],
+        ],
+        // expected job order
+        ['1'],
+    ],
+    // mutex and rank allows only the top-ranked job to run
+    [
+        // buffered jobs
+        [
+            ['name' => '1', 'mutex_id' => 'a', 'job_rank' => 5],
+            ['name' => '2', 'mutex_id' => 'a', 'job_rank' => 1],
+        ],
+        // expected job order
+        ['2'],
+    ],
+    // run after in the future prevents job from being queued now
+    [
+        // buffered jobs
+        [
+            ['name' => '1', 'run_after' => 60],
+            ['name' => '2', 'run_after' => 60],
+        ],
+        // expected job order
+        [],
+    ],
+    // mutexed run after in the future allows other mutexed job to become queued
+    [
+        // buffered jobs
+        [
+            ['name' => '1', 'mutex_id' => 'a', 'run_after' => 60],
+            ['name' => '2', 'mutex_id' => 'a'],
+        ],
+        // expected job order
+        ['2'],
+    ],
+    // rank does not allow future jobs to affect current jobs
+    [
+        // buffered jobs
+        [
+            ['name' => '1', 'mutex_id' => 'a', 'job_rank' => 1, 'run_after' => 60],
+            ['name' => '2', 'mutex_id' => 'a', 'job_rank' => 5],
+        ],
+        // expected job order
+        ['2'],
+    ],
+];
