58cff774c30e6e78211f298521983a674eb470d8
Matt Light <matt.light@lightdatasys.com>
2016-10-18 00:17:15 -0700

    Add Consumer to supplement QueueFactory
    QueueFactory is going away soon in favor of separate
    Producers/Consumers

---
+++ tests/src/Hodor/MessageQueue/ConsumerQueueTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Hodor\MessageQueue;
+
+use PHPUnit_Framework_TestCase;
+
+/**
+ * @coversDefaultClass Hodor\MessageQueue\ConsumerQueue
+ */
+class ConsumerQueueTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers ::__construct
+     * @covers ::consume
+     */
+    public function testConsumerReceivesExpectedCallback()
+    {
+        $expected_callback = function ($message) {};
+        $consumer = function (callable $callback) use ($expected_callback) {
+            $this->assertSame($expected_callback, $callback);
+        };
+
+        $consumer_queue = new ConsumerQueue($consumer);
+        $consumer_queue->consume($expected_callback);
+    }
+}
